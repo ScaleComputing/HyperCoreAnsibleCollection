5a8e3924445fad6aeda959782cdcb85cbd9a8471
Jure Medvesek <jure.medvesek@xlab.si>
2022-09-08 16:22:54 +0200

    Minimal docs example

---
+++ docs/source/conf.py
@@ -0,0 +1,17 @@
+project = "Hypercore Ansible Collection"
+copyright = "2022, XLAB Steampunk"
+author = "XLAB Steampunk"
+
+extensions = [
+    "sphinx_rtd_theme",
+]
+exclude_patterns = []
+
+html_theme = "sphinx_rtd_theme"
+html_context = {
+    "display_github": False,
+    # "github_user": "",
+    # "github_repo": "",
+    # "github_version": "master",
+    "conf_py_path": "/docs/source/",
+}
