e90ace21c0d871cf926be276c4a344032b7c6bcb
Justin Cinkelj <justin.cinkelj@xlab.si>
2022-10-27 22:25:35 +0200

    Test caching different endpoints
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- tests/unit/plugins/module_utils/test_rest_client_CachedRestClient.py
+++ tests/unit/plugins/module_utils/test_rest_client_CachedRestClient.py
@@ -39,7 +39,6 @@ class TestCachedRestClient:
         ],
     )
     def test_list_records(self, mocker, swap_query_order):
-        # records = self.client.get(path=endpoint, timeout=timeout).json
         endpoint = "url1"
         data = '[{"name": "vm0"}, {"name": "vm1"}]'
         #
@@ -79,3 +78,41 @@ class TestCachedRestClient:
         records = cached_client.list_records(endpoint, query1)
         assert records == json.loads(expected_data1)
         assert client_mock.call_count == 1
+
+    def test_list_records_different_endpoint(self, mocker):
+        # Check calling with different endpoint does return different data
+        endpoint0 = "vms"
+        endpoint1 = "disks"
+        data0 = '[{"name": "vm0"}]'
+        data1 = '[{"disk_type": "ide"}]'
+
+        def mockup_client_get(path, timeout):
+            if path == endpoint0:
+                return client.Response(200, data0, "")
+            else:
+                return client.Response(200, data1, "")
+
+        client_obj = client.Client("https://thehost", "user", "pass")
+        cached_client = rest_client.CachedRestClient(client=client_obj)
+        client_mock = mocker.patch.object(cached_client.client, "get")
+        client_mock.side_effect = mockup_client_get
+
+        # on first call, we get something back
+        records = cached_client.list_records(endpoint0, None)
+        assert records == json.loads(data0)
+        assert client_mock.call_count == 1
+
+        # on second call, we get something else back
+        records = cached_client.list_records(endpoint1, None)
+        assert records == json.loads(data1)
+        # and one extra HTTP request was needed
+        assert client_mock.call_count == 2
+
+        # try both endpoints once more
+        records = cached_client.list_records(endpoint0, None)
+        assert records == json.loads(data0)
+        assert client_mock.call_count == 2
+        #
+        records = cached_client.list_records(endpoint1, None)
+        assert records == json.loads(data1)
+        assert client_mock.call_count == 2
