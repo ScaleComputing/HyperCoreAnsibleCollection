afb7fa54d414b5fc858d90b98c50dc636fb095e6
Domen <domen.dobnikar@xlab.si>
2022-11-16 13:37:33 +0100

    smb cleanup

--- tests/integration/smb_cleanup.sh
+++ tests/integration/smb_cleanup.sh
@@ -5,11 +5,6 @@ ${SMB_USERNAME}
 ${SMB_SHARE}
 ${SMB_SERVER}
 
-SC_HOST=$(( $SMB_SERVER | sed 's/http/hTTp/' ))
-
-echo $SC_HOST
-echo $(( ${SMB_SERVER} | sed 's/http/hTTp/' ))
-echo $(( $SMB_SERVER | sed 's/http/hTTp/' ))
 
 sshpass -p root ssh root@${SMB_SERVER}
 echo "BLA"
