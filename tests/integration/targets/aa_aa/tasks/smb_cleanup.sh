9a79cf3492dbc73b88345b08e4736bbe83730c99
Domen <domen.dobnikar@xlab.si>
2022-11-17 15:35:14 +0100

    smb cleanup script

---
+++ tests/integration/targets/aa_aa/tasks/smb_cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+files=($(smbclient //$SMB_SERVER$SMB_SHARE -U $SMB_USERNAME $SMB_PASSWORD -N -c ls | awk '{print $1}'))
+#dates=($(smbclient //$SMB_SERVER$SMB_SHARE -U $SMB_USERNAME $SMB_PASSWORD -N -c ls -l | awk '{print $4}'))
+
+
+for file in ${files[@]}
+do
+    if [ $file != '.' ] && [ $file != '..' ]
+    then
+        echo "Attempting to delete:" $file
+        smbclient //$SMB_SERVER$SMB_SHARE -U $SMB_USERNAME $SMB_PASSWORD -N -c 'deltree '$file''
+    fi
+done
