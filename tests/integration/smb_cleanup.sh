f3b566ef5552ec9a789a238163e9fa6444f5dbca
Domen <domen.dobnikar@xlab.si>
2022-11-23 10:49:36 +0100

    Adding timestamp logic to smb cleanup

--- tests/integration/smb_cleanup.sh
+++ tests/integration/smb_cleanup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 files=($(smbclient //$SMB_SERVER$SMB_SHARE -U $SMB_USERNAME $SMB_PASSWORD -N -c ls | awk '{print $1}'))
-#dates=($(smbclient //$SMB_SERVER$SMB_SHARE -U $SMB_USERNAME $SMB_PASSWORD -N -c ls -l | awk '{print $4}'))
+dates=($(smbclient //$SMB_SERVER$SMB_SHARE -U $SMB_USERNAME $SMB_PASSWORD -N -c ls -l | awk '{print $5":"$6":"$8}'))
+today_date=$(date +'%b:%d:%Y')
 
-for file in ${files[@]}
+echo "Todays date:" $today_date
+
+length=${#files[@]}-1
+for (( j=0; j<length; j++ ));
 do
-    if [ $file != '.' ] && [ $file != '..' ]
+    # Delete files that are at least one day old, in order to not crash other integration tests
+    if [ ${files[j]} != '.' ] && [ ${files[j]} != '..' ] && [ ${files[j]} != '.deleted' ] && [ ${dates[j]} != $today_date ] 
     then
-        echo "Attempting to delete:" $file
+        echo "Attempting to delete:" ${files[j]} "with timestamp:" ${dates[j]}
         smbclient //$SMB_SERVER$SMB_SHARE -U $SMB_USERNAME $SMB_PASSWORD -N -c 'deltree '$file''
     fi
 done
