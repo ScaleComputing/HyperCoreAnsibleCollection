ef3a30171dcfdea1e35edc57f7d8ebe3a6f011d9
Domen <domen.dobnikar@xlab.si>
2022-12-01 16:24:17 +0100

    smb client

--- tests/integration/cleanup/smb_cleanup.sh
+++ tests/integration/cleanup/smb_cleanup.sh
@@ -15,19 +15,4 @@ SMBCLIENTCOMMANDS
 
 exit 0
 
-files=($(smbclient //$SMB_SERVER$SMB_SHARE -U ${username[0]}%$SMB_PASSWORD -c ls | awk '{print $1}'))
-dates=($(smbclient //$SMB_SERVER$SMB_SHARE -U ${username[1]}%$SMB_PASSWORD -c ls -l | awk '{print $5":"$6":"$8}'))
-today_date=$(date +'%b:%d:%Y')
 
-echo "Todays date:" $today_date
-
-length=${#files[@]}-1
-for (( j=0; j<length; j++ ));
-do
-    # Delete files that are at least one day old, in order to not crash other integration tests
-    if [ ${files[j]} != '.' ] && [ ${files[j]} != '..' ] && [ ${files[j]} != '.deleted' ] && [ ${files[j]} != '.deleted' ] && [ ${dates[j]} != $today_date ] 
-    then
-        echo "Attempting to delete:" ${files[j]} "with timestamp:" ${dates[j]}
-        smbclient //$SMB_SERVER$SMB_SHARE -U ${username[1]}%$SMB_PASSWORD -c 'deltree '$file''
-    fi
-done
