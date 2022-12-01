36a9537b88314be83253ef5665ecaa871d993dda
Domen <domen.dobnikar@xlab.si>
2022-12-01 16:05:28 +0100

    smb cleanup

--- tests/integration/cleanup/smb_cleanup.sh
+++ tests/integration/cleanup/smb_cleanup.sh
@@ -6,7 +6,7 @@
 # password = $4
 
 # username is provided as domain;username
-IFS=';' read -ra username <<< "$3"
+
 
 smbclient //192.168.1.248/ansibleci -U pm-edge/administrator%Scale2020! -W pm-edge << SMBCLIENTCOMMANDS
 ls
