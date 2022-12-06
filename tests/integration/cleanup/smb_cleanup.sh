e1848945075f91e42d8c3640505a04179c272876
Domen <domen.dobnikar@xlab.si>
2022-12-06 13:15:44 +0100

    smb cleanup

--- tests/integration/cleanup/smb_cleanup.sh
+++ tests/integration/cleanup/smb_cleanup.sh
@@ -9,7 +9,7 @@
 IFS=';' read -ra username <<< "$3"
 
 
-smbclient //$1$2 -U ${username[1]}%"Scale2020!" << SMBCLIENTCOMMANDS
+smbclient //$1$2 -U "administrator"%"Scale2020!" << SMBCLIENTCOMMANDS
 ls
 SMBCLIENTCOMMANDS
 
