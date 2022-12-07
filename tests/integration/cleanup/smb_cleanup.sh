f546d1a86d27ccf321c7c20cf8b35bcb0230745f
Domen <domen.dobnikar@xlab.si>
2022-12-07 11:35:32 +0100

    smb cleanup

--- tests/integration/cleanup/smb_cleanup.sh
+++ tests/integration/cleanup/smb_cleanup.sh
@@ -18,37 +18,34 @@ echo ${username[0]} ":" ${username[1]}
 # dates = $6
 # folder = $7
 delete_files () { 
-    if [ $7 == 'export' ]
-    then
-        folder='integration-test-vm-export'
-    else
-        folder='integration-test-vm-import'
-    fi
-    smbclient //$1$2 -U $3%$4 -D $folder << SMBCLIENTCOMMANDS
+    smbclient //$1$2 -U $3%$4 -D $7 << SMBCLIENTCOMMANDS
     ls
 SMBCLIENTCOMMANDS
 
     today_date=$(date +'%b:%d:%Y')
     echo "Todays date:" $today_date
     length=$($5[@])-1
+    echo $length
     for (( j=0; j<length; j++ ));
     do
         # Delete files that are at least one day old, in order to not crash other integration tests
-        if [ ${5[j]} != '.' ] && [ ${5[j]} != '..' ] && [ ${5[j]} != '.deleted' ] && [ ${6[j]} != $today_date ] 
+        if [ ${$5[j]} != '.' ] && [ ${$5[j]} != '..' ] && [ ${$5[j]} != '.deleted' ] && [ ${$6[j]} != $today_date ] 
         then
-            echo "Attempting to delete:" ${5[j]} "with timestamp:" ${6[j]}
-            smbclient //$1$2 -U $3%$4 -D $folder -c 'deltree '${5[j]}''
+            echo "Attempting to delete:" ${$5[j]} "with timestamp:" ${$6[j]}
+            smbclient //$1$2 -U $3%$4 -D $7 -c 'deltree '${$5[j]}''
         fi
 done
  }
 
-
-files=($(smbclient //$1$2 -U ${username[1]}%$4 -D 'integration-test-vm-export' -c ls | awk '{print $1}'))
-dates=($(smbclient //$1$2 -U ${username[1]}%$4 -D 'integration-test-vm-export' -c ls -l | awk '{print $5":"$6":"$8}'))
-
+echo "Getting variables for export"
+folder='integration-test-vm-export'
+files=($(smbclient //$1$2 -U ${username[1]}%$4 -D $folder -c ls | awk '{print $1}'))
+dates=($(smbclient //$1$2 -U ${username[1]}%$4 -D $folder -c ls -l | awk '{print $5":"$6":"$8}'))
+echo "first function call"
 delete_files $1 $2 $username $4 $files $dates 'export'
 
-files=($(smbclient //$1$2 -U ${username[1]}%$4 -D 'integration-test-vm-import' -c ls | awk '{print $1}'))
-dates=($(smbclient //$1$2 -U ${username[1]}%$4 -D 'integration-test-vm-import' -c ls -l | awk '{print $5":"$6":"$8}'))
+folder='integration-test-vm-import'
+files=($(smbclient //$1$2 -U ${username[1]}%$4 -D $folder -c ls | awk '{print $1}'))
+dates=($(smbclient //$1$2 -U ${username[1]}%$4 -D $folder -c ls -l | awk '{print $5":"$6":"$8}'))
 
-delete_files $1 $2 $username $4 $files $dates 'import'
+delete_files $1 $2 $username $4 $files $dates $folder
