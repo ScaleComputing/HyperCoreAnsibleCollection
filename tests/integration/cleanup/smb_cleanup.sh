233ab8d0ba2c5f012deb8fabeff65aef4386c33b
Domen <domen.dobnikar@xlab.si>
2022-12-07 10:33:01 +0100

    smbcleanup

--- tests/integration/cleanup/smb_cleanup.sh
+++ tests/integration/cleanup/smb_cleanup.sh
@@ -13,6 +13,10 @@ smbclient //$1$2 -U "administrator"%"Scale2020!" -D 'integration-test-vm-export'
 ls
 SMBCLIENTCOMMANDS
 
+smbclient //$1$2 -U "administrator"%"Scale2020!" -D 'integration-test-vm-import' << SMBCLIENTCOMMANDS
+ls
+SMBCLIENTCOMMANDS
+
 export_files=($(smbclient //$1$2 -U "administrator"%"Scale2020!" -D 'integration-test-vm-export' -c ls | awk '{print $1}'))
 export_dates=($(smbclient //$1$2 -U "administrator"%"Scale2020!" -D 'integration-test-vm-export' -c ls -l | awk '{print $5":"$6":"$8}'))
 import_files=($(smbclient //$1$2 -U "administrator"%"Scale2020!" -D 'integration-test-vm-import' -c ls | awk '{print $1}'))
@@ -32,5 +36,16 @@ do
     then
         echo "Attempting to delete:" ${export_files[j]} "with timestamp:" ${export_dates[j]}
         smbclient //$1$2 -U "administrator"%"Scale2020!" -D 'integration-test-vm-export' -c 'deltree '${export_files[j]}''
+    fi
+done
+
+length=${#import_files[@]}-1
+for (( j=0; j<length; j++ ));
+do
+    # Delete files that are at least one day old, in order to not crash other integration tests
+    if [ ${import_files[j]} != '.' ] && [ ${import_files[j]} != '..' ] && [ ${import_files[j]} != '.deleted' ] && [ ${import_dates[j]} != $today_date ] 
+    then
+        echo "Attempting to delete:" ${import_files[j]} "with timestamp:" ${import_dates[j]}
+        smbclient //$1$2 -U "administrator"%"Scale2020!" -D 'integration-test-vm-import' -c 'deltree '${import_files[j]}''
     fi
 done
