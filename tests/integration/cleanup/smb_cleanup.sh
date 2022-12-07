cc6b90b30761379ce9b7f1a1bfdbfa1854a340c7
Domen <domen.dobnikar@xlab.si>
2022-12-07 09:50:24 +0100

    smb cleanup

--- tests/integration/cleanup/smb_cleanup.sh
+++ tests/integration/cleanup/smb_cleanup.sh
@@ -13,10 +13,10 @@ smbclient //$1$2 -U "administrator"%"Scale2020!" << SMBCLIENTCOMMANDS
 ls
 SMBCLIENTCOMMANDS
 
-files=($(smbclient //$1$2 -U "administrator"%"Scale2020!" << SMBCLIENTCOMMANDS
+files=($(smbclient //$1$2 -U "administrator"%"Scale2020!")) << SMBCLIENTCOMMANDS
 cd integration-test-vm-export
 ls
-SMBCLIENTCOMMANDS))
+SMBCLIENTCOMMANDS
 
 echo ${files[1]}
 
