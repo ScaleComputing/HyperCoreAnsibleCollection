f5afdaba333a570abee3c8aad727deb122f92da3
Polona Mihalič <polona.mihalic@xlab.si>
2023-02-14 10:03:42 +0100

    Add min_python_version variable for pytest skipif

--- tests/unit/plugins/module_utils/test_remote_cluster.py
+++ tests/unit/plugins/module_utils/test_remote_cluster.py
@@ -9,9 +9,13 @@ import pytest
 from ansible_collections.scale_computing.hypercore.plugins.module_utils.remote_cluster import (
     RemoteCluster,
 )
+from ansible_collections.scale_computing.hypercore.plugins.module_utils.utils import (
+    MIN_PYTHON_VERSION,
+)
 
 pytestmark = pytest.mark.skipif(
-    sys.version_info < (2, 7), reason="requires python2.7 or higher"
+    sys.version_info < MIN_PYTHON_VERSION,
+    reason=f"requires python{MIN_PYTHON_VERSION[0]}.{MIN_PYTHON_VERSION[1]} or higher",
 )
 
 
--- tests/unit/plugins/module_utils/test_virtual_disk.py
+++ tests/unit/plugins/module_utils/test_virtual_disk.py
@@ -15,9 +15,13 @@ import pytest
 from ansible_collections.scale_computing.hypercore.plugins.module_utils.virtual_disk import (
     VirtualDisk,
 )
+from ansible_collections.scale_computing.hypercore.plugins.module_utils.utils import (
+    MIN_PYTHON_VERSION,
+)
 
 pytestmark = pytest.mark.skipif(
-    sys.version_info < (2, 7), reason="requires2.7 or higher"
+    sys.version_info < MIN_PYTHON_VERSION,
+    reason=f"requires python{MIN_PYTHON_VERSION[0]}.{MIN_PYTHON_VERSION[1]} or higher",
 )
 
 
