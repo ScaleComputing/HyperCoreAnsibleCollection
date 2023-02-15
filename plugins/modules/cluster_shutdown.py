94dd4fab9bda1855ab3932463488583415fccb70
Polona Mihalič <polona.mihalic@xlab.si>
2023-02-15 14:03:57 +0100

    Update return record

--- plugins/modules/cluster_shutdown.py
+++ plugins/modules/cluster_shutdown.py
@@ -38,8 +38,16 @@ EXAMPLES = r"""
     force_shutdown: true
 """
 
-RETURN = r""" # WHAT TO RETURN?
+RETURN = r"""
 record:
+  description:
+    - Cluster that was shutdown.
+  returned: success
+  type: dict
+  sample:
+    - icos_version: 9.2.11.210763
+      name: PUB5
+      uuid: a5d9148c-37f7-4b43-843c-196751d3c050
 """
 
 from ansible.module_utils.basic import AnsibleModule
@@ -48,10 +56,20 @@ from ..module_utils import arguments, errors
 from ..module_utils.client import Client
 from ..module_utils.rest_client import RestClient
 from ..module_utils.cluster import Cluster
+from ..module_utils.typed_classes import TypedDiff
+from typing import Tuple, Dict
 
 
-def run(module: AnsibleModule, rest_client: RestClient) -> None:  # WHAT TO RETURN??
+def run(
+    module: AnsibleModule, rest_client: RestClient
+) -> Tuple[bool, Dict[None, None], TypedDiff]:
+    cluster = Cluster.get(rest_client)
     Cluster.shutdown(rest_client, module.params["force_shutdown"])
+    return (
+        True,
+        dict(),
+        dict(before=cluster.to_ansible(), after=None),
+    )  # WHAT TO RETURN?
 
 
 def main() -> None:
@@ -66,8 +84,8 @@ def main() -> None:
     try:
         client = Client.get_client(module.params["cluster_instance"])
         rest_client = RestClient(client)
-        run(module, rest_client)
-        module.exit_json()
+        record = run(module, rest_client)
+        module.exit_json(record=record)
     except errors.ScaleComputingError as e:
         module.fail_json(msg=str(e))
 
