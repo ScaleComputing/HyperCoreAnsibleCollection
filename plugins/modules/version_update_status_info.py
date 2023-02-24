f2903867cc530c0b149bf4970cbaca1fcd5d8d0b
Polona Mihalič <polona.mihalic@xlab.si>
2023-02-24 17:12:03 +0100

    Add version_update_status_info module

---
+++ plugins/modules/version_update_status_info.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2022, XLAB Steampunk <steampunk@xlab.si>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+module: version_update_status_info
+
+author:
+  - Polona Mihalič (@PolonaM)
+short_description: Returns status of the latest update applied.
+description:
+  - Returns status of the latest update applied. Possible values are COMPLETE, IN PROGRESS, TERMINATING, TERMINATED # TODO: check for aditional ones
+version_added: 1.2.0
+extends_documentation_fragment:
+  - scale_computing.hypercore.cluster_instance
+seealso:
+  - module: scale_computing.hypercore.version_update
+  - module: scale_computing.hypercore.version_update_info
+"""
+
+EXAMPLES = r"""
+- name: Get status of the latest update applied
+  scale_computing.hypercore.version_update_status_info:
+  register: result
+"""
+
+RETURN = r"""
+record:
+  description:
+    - Status of the latest update applied
+  returned: success
+  type: dict
+  sample:
+    - from_build: 207183
+      percent: 100
+      status: COMPLETE
+      status_details: Update Complete. Press 'Reload' to reconnect
+      to_build: 209840
+      to_version: 9.1.18.209840
+"""
+
+from ansible.module_utils.basic import AnsibleModule
+
+from ..module_utils import arguments, errors
+from ..module_utils.rest_client import RestClient
+from ..module_utils.client import Client
+from ..module_utils.hypercore_version import UpdateStatus
+from ..module_utils.typed_classes import TypedUpdateStatusToAnsible
+from typing import List, Union
+
+
+def run(rest_client: RestClient) -> TypedUpdateStatusToAnsible:
+    return UpdateStatus.get(rest_client).to_ansible()
+
+
+def main() -> None:
+    module = AnsibleModule(
+        supports_check_mode=True,
+        argument_spec=dict(
+            arguments.get_spec("cluster_instance"),
+        ),
+    )
+
+    try:
+        client = Client.get_client(module.params["cluster_instance"])
+        rest_client = RestClient(client)
+        record = run(rest_client)
+        module.exit_json(record=record)
+
+    except errors.ScaleComputingError as e:
+        module.fail_json(msg=str(e))
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/unit/plugins/modules/test_version_update_status_info.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# # Copyright: (c) 2023, XLAB Steampunk <steampunk@xlab.si>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+import sys
+
+import pytest
+
+from ansible_collections.scale_computing.hypercore.plugins.modules import (
+    version_update_status_info,
+)
+from ansible_collections.scale_computing.hypercore.plugins.module_utils.utils import (
+    MIN_PYTHON_VERSION,
+)
+
+pytestmark = pytest.mark.skipif(
+    sys.version_info < MIN_PYTHON_VERSION,
+    reason=f"requires python{MIN_PYTHON_VERSION[0]}.{MIN_PYTHON_VERSION[1]} or higher",
+)
+
+
+class TestRun:
+    def test_run(self, rest_client, mocker):
+        mocker.patch(
+            "ansible_collections.scale_computing.hypercore.plugins.modules.version_update_status_info.UpdateStatus.get"
+        ).return_value = dict(
+            from_build="207183",
+            percent="100",
+            status="COMPLETE",
+            status_details="Update Complete. Press 'Reload' to reconnect",
+            to_build="209840",
+            to_version="9.1.18.209840",
+        )
+
+        record = version_update_status_info.run(rest_client)
+
+        assert record == dict(
+            from_build="207183",
+            percent="100",
+            status="COMPLETE",
+            status_details="Update Complete. Press 'Reload' to reconnect",
+            to_build="209840",
+            to_version="9.1.18.209840",
+        )
