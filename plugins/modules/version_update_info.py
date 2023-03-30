d3ffb7ba89f995a022826c11ca401b90e91ea4a9
Polona Mihalič <polona.mihalic@xlab.si>
2023-03-30 23:00:56 +0200

    Add option select next or latest to version_update_info

--- plugins/modules/version_update_info.py
+++ plugins/modules/version_update_info.py
@@ -22,11 +22,29 @@ extends_documentation_fragment:
 seealso:
   - module: scale_computing.hypercore.version_update
   - module: scale_computing.hypercore.version_update_status_info
+options:
+  select:
+    description:
+      - List desired version update from list of updates
+      - If C(next), then version with the lowest number in the list of available updates is returned
+      - If C(latest), then version with the highest number in the list of available updates is returned
+    type: str
+    choices: [ next, latest ]
 """
 
 EXAMPLES = r"""
 - name: Get a list of updates
   scale_computing.hypercore.version_update_info:
+  register: result
+
+- name: Get a list of updates and next update
+  scale_computing.hypercore.version_update_info:
+    select: next
+  register: result
+
+- name: Get a list of updates and latest update
+  scale_computing.hypercore.version_update_info:
+    select: latest
   register: result
 """
 
@@ -36,15 +54,77 @@ records:
     - A list of updates that can be applied to this cluster.
   returned: success
   type: list
-  sample:
-    - uuid: 9.2.11.210763
-      description: 9.2.11 General Availability
-      change_log: ...Please allow between 20-40 minutes per node for the update to complete...
-      build_id: 210763
-      major_version: 9
-      minor_version: 2
-      revision: 11
-      timestamp: 0
+  contains:
+    uuid:
+      description: Unique identifier in format majorVersion.minorVersion.revision.buildID
+      type: str
+      sample: 9.2.11.210763
+    description:
+      description: Human-readable name for the update
+      type: str
+      sample: 9.2.11 General Availability
+    change_log:
+      description: Description of all changes that are in this update, in HTML format
+      type: str
+      sample: ...Please allow between 20-40 minutes per node for the update to complete...
+    build_id:
+      description: ID of the build which corresponds to this update
+      type: int
+      sample: 210763
+    major_version:
+      description: Major version number
+      type: int
+      sample: 9
+    minor_version:
+      description: Minor version number
+      type: int
+      sample: 2
+    revision:
+      description: Revision number
+      type: int
+      sample: 11
+    timestamp:
+      description: Unix timestamp when the update was released
+      type: int
+      sample: 0
+record:
+  description:
+    - Next or latest update from list of updates, when option I(select) is specified
+  returned: success
+  type: dict
+  contains:
+    uuid:
+      description: Unique identifier in format majorVersion.minorVersion.revision.buildID
+      type: str
+      sample: 9.2.11.210763
+    description:
+      description: Human-readable name for the update
+      type: str
+      sample: 9.2.11 General Availability
+    change_log:
+      description: Description of all changes that are in this update, in HTML format
+      type: str
+      sample: ...Please allow between 20-40 minutes per node for the update to complete...
+    build_id:
+      description: ID of the build which corresponds to this update
+      type: int
+      sample: 210763
+    major_version:
+      description: Major version number
+      type: int
+      sample: 9
+    minor_version:
+      description: Minor version number
+      type: int
+      sample: 2
+    revision:
+      description: Revision number
+      type: int
+      sample: 11
+    timestamp:
+      description: Unix timestamp when the update was released
+      type: int
+      sample: 0
 """
 
 from ansible.module_utils.basic import AnsibleModule
@@ -54,14 +134,38 @@ from ..module_utils.rest_client import RestClient
 from ..module_utils.client import Client
 from ..module_utils.hypercore_version import Update
 from ..module_utils.typed_classes import TypedUpdateToAnsible
-from typing import List, Optional
+from typing import List, Optional, Tuple
+import operator
 
 
-def run(rest_client: RestClient) -> List[Optional[TypedUpdateToAnsible]]:
-    return [
+def select_next_or_latest(
+    list_of_updates: List[TypedUpdateToAnsible], next_or_latest: str
+) -> Optional[TypedUpdateToAnsible]:
+    if not list_of_updates:
+        return None
+    sorted_list = list_of_updates.copy()  # so that we keep original records
+    sorted_list.sort(
+        key=operator.itemgetter(
+            "major_version", "minor_version", "revision", "build_id"
+        )
+    )
+    if next_or_latest == "next":
+        return sorted_list[0]
+    else:
+        return sorted_list[-1]
+
+
+def run(
+    module: AnsibleModule, rest_client: RestClient
+) -> Tuple[List[Optional[TypedUpdateToAnsible]], Optional[TypedUpdateToAnsible]]:
+    record = None
+    records = [
         Update.from_hypercore(hypercore_data=hypercore_dict).to_ansible()  # type: ignore
         for hypercore_dict in rest_client.list_records("/rest/v1/Update")
     ]
+    if module.params["select"]:
+        record = select_next_or_latest(records, module.params["select"])
+    return records, record
 
 
 def main() -> None:
@@ -69,14 +173,15 @@ def main() -> None:
         supports_check_mode=True,
         argument_spec=dict(
             arguments.get_spec("cluster_instance"),
+            select=dict(type="str", choices=["next", "latest"]),
         ),
     )
 
     try:
         client = Client.get_client(module.params["cluster_instance"])
         rest_client = RestClient(client)
-        records = run(rest_client)
-        module.exit_json(changed=False, records=records)
+        records, record = run(module, rest_client)
+        module.exit_json(changed=False, records=records, record=record)
 
     except errors.ScaleComputingError as e:
         module.fail_json(msg=str(e))
--- tests/unit/plugins/modules/test_version_update_info.py
+++ tests/unit/plugins/modules/test_version_update_info.py
@@ -24,32 +24,173 @@ pytestmark = pytest.mark.skipif(
 )
 
 
+class TestSelectNextOrLatest:
+    def test_select(self):
+        list_of_updates = [
+            {
+                "uuid": "9.2.11.210764",
+                "build_id": 210764,
+                "major_version": 9,
+                "minor_version": 2,
+                "revision": 11,
+            },
+            {
+                "uuid": "9.2.12.210763",
+                "build_id": 210763,
+                "major_version": 9,
+                "minor_version": 2,
+                "revision": 12,
+            },
+            {
+                "uuid": "9.2.11.210763",
+                "build_id": 210763,
+                "major_version": 9,
+                "minor_version": 2,
+                "revision": 11,
+            },
+            {
+                "uuid": "10.2.11.210763",
+                "build_id": 210763,
+                "major_version": 10,
+                "minor_version": 2,
+                "revision": 11,
+            },
+            {
+                "uuid": "9.3.11.210763",
+                "build_id": 210763,
+                "major_version": 9,
+                "minor_version": 3,
+                "revision": 11,
+            },
+        ]
+        next = version_update_info.select_next_or_latest(list_of_updates, "next")
+        latest = version_update_info.select_next_or_latest(list_of_updates, "latest")
+
+        assert next == {
+            "uuid": "9.2.11.210763",
+            "build_id": 210763,
+            "major_version": 9,
+            "minor_version": 2,
+            "revision": 11,
+        }
+        assert latest == {
+            "uuid": "10.2.11.210763",
+            "build_id": 210763,
+            "major_version": 10,
+            "minor_version": 2,
+            "revision": 11,
+        }
+
+
 class TestRun:
-    def test_run(self, rest_client, mocker):
-        rest_client.list_records.return_value = [
-            dict(
-                uuid="9.2.11.210763",
-                description="description",
-                changeLog="change log",
-                buildID=210763,
-                majorVersion=9,
-                minorVersion=2,
-                revision=11,
-                timestamp=1676920067,
+    @pytest.mark.parametrize(
+        ("select", "select_record"),
+        [
+            (None, None),
+            (
+                "next",
+                {
+                    "uuid": "9.2.11.210763",
+                    "description": "description",
+                    "change_log": "change log",
+                    "build_id": 210763,
+                    "major_version": 9,
+                    "minor_version": 2,
+                    "revision": 11,
+                    "timestamp": 1676920067,
+                },
+            ),
+            (
+                "latest",
+                {
+                    "uuid": "10.2.11.210763",
+                    "description": "description",
+                    "change_log": "change log",
+                    "build_id": 210763,
+                    "major_version": 10,
+                    "minor_version": 2,
+                    "revision": 11,
+                    "timestamp": 1676920067,
+                },
+            ),
+        ],
+    )
+    def test_run(self, rest_client, create_module, select, select_record):
+        module = create_module(
+            params=dict(
+                cluster_instance=dict(
+                    host="https://0.0.0.0",
+                    username=None,
+                    password=None,
+                ),
+                select=select,
             )
+        )
+        rest_client.list_records.return_value = [
+            {
+                "uuid": "9.2.12.210763",
+                "description": "description",
+                "changeLog": "change log",
+                "buildID": 210763,
+                "majorVersion": 9,
+                "minorVersion": 2,
+                "revision": 12,
+                "timestamp": 1676920067,
+            },
+            {
+                "uuid": "9.2.11.210763",
+                "description": "description",
+                "changeLog": "change log",
+                "buildID": 210763,
+                "majorVersion": 9,
+                "minorVersion": 2,
+                "revision": 11,
+                "timestamp": 1676920067,
+            },
+            {
+                "uuid": "10.2.11.210763",
+                "description": "description",
+                "changeLog": "change log",
+                "buildID": 210763,
+                "majorVersion": 10,
+                "minorVersion": 2,
+                "revision": 11,
+                "timestamp": 1676920067,
+            },
         ]
 
-        records = version_update_info.run(rest_client)
+        records, record = version_update_info.run(module, rest_client)
 
         assert records == [
-            dict(
-                uuid="9.2.11.210763",
-                description="description",
-                change_log="change log",
-                build_id=210763,
-                major_version=9,
-                minor_version=2,
-                revision=11,
-                timestamp=1676920067,
-            )
+            {
+                "uuid": "9.2.12.210763",
+                "description": "description",
+                "change_log": "change log",
+                "build_id": 210763,
+                "major_version": 9,
+                "minor_version": 2,
+                "revision": 12,
+                "timestamp": 1676920067,
+            },
+            {
+                "uuid": "9.2.11.210763",
+                "description": "description",
+                "change_log": "change log",
+                "build_id": 210763,
+                "major_version": 9,
+                "minor_version": 2,
+                "revision": 11,
+                "timestamp": 1676920067,
+            },
+            {
+                "uuid": "10.2.11.210763",
+                "description": "description",
+                "change_log": "change log",
+                "build_id": 210763,
+                "major_version": 10,
+                "minor_version": 2,
+                "revision": 11,
+                "timestamp": 1676920067,
+            },
         ]
+        assert record == select_record
