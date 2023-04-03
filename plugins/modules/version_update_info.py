63f1868b139978cd53f1aeea3de539960ea4f740
Polona Mihalič <polona.mihalic@xlab.si>
2023-04-03 12:57:33 +0200

    Remove option select and instead of record add next and latest to return value

--- plugins/modules/version_update_info.py
+++ plugins/modules/version_update_info.py
@@ -22,36 +22,18 @@ extends_documentation_fragment:
 seealso:
   - module: scale_computing.hypercore.version_update
   - module: scale_computing.hypercore.version_update_status_info
-options:
-  select:
-    description:
-      - List desired version update from list of updates
-      - If C(next), then version with the lowest number in the list of available updates is returned
-      - If C(latest), then version with the highest number in the list of available updates is returned
-    type: str
-    choices: [ next, latest ]
 """
 
 EXAMPLES = r"""
 - name: Get a list of updates
   scale_computing.hypercore.version_update_info:
   register: result
-
-- name: Get a list of updates and next update
-  scale_computing.hypercore.version_update_info:
-    select: next
-  register: result
-
-- name: Get a list of updates and latest update
-  scale_computing.hypercore.version_update_info:
-    select: latest
-  register: result
 """
 
 RETURN = r"""
 records:
   description:
-    - A list of updates that can be applied to this cluster.
+    - In ascending order sorted list of updates that can be applied to this cluster.
   returned: success
   type: list
   contains:
@@ -87,9 +69,47 @@ records:
       description: Unix timestamp when the update was released
       type: int
       sample: 0
-record:
+next:
   description:
-    - Next or latest update from list of updates, when option I(select) is specified
+    - Version with the lowest number in the list of available updates
+  returned: success
+  type: dict
+  contains:
+    uuid:
+      description: Unique identifier in format majorVersion.minorVersion.revision.buildID
+      type: str
+      sample: 9.2.11.210763
+    description:
+      description: Human-readable name for the update
+      type: str
+      sample: 9.2.11 General Availability
+    change_log:
+      description: Description of all changes that are in this update, in HTML format
+      type: str
+      sample: ...Please allow between 20-40 minutes per node for the update to complete...
+    build_id:
+      description: ID of the build which corresponds to this update
+      type: int
+      sample: 210763
+    major_version:
+      description: Major version number
+      type: int
+      sample: 9
+    minor_version:
+      description: Minor version number
+      type: int
+      sample: 2
+    revision:
+      description: Revision number
+      type: int
+      sample: 11
+    timestamp:
+      description: Unix timestamp when the update was released
+      type: int
+      sample: 0
+latest:
+  description:
+    - Version with the highest number in the list of available updates
   returned: success
   type: dict
   contains:
@@ -138,34 +158,25 @@ from typing import List, Optional, Tuple
 import operator
 
 
-def select_next_or_latest(
-    list_of_updates: List[TypedUpdateToAnsible], next_or_latest: str
-) -> Optional[TypedUpdateToAnsible]:
-    if not list_of_updates:
-        return None
-    sorted_list = list_of_updates.copy()  # so that we keep original records
-    sorted_list.sort(
-        key=operator.itemgetter(
-            "major_version", "minor_version", "revision", "build_id"
-        )
-    )
-    if next_or_latest == "next":
-        return sorted_list[0]
-    else:
-        return sorted_list[-1]
-
-
 def run(
-    module: AnsibleModule, rest_client: RestClient
-) -> Tuple[List[Optional[TypedUpdateToAnsible]], Optional[TypedUpdateToAnsible]]:
-    record = None
+    rest_client: RestClient,
+) -> Tuple[
+    List[Optional[TypedUpdateToAnsible]],
+    Optional[TypedUpdateToAnsible],
+    Optional[TypedUpdateToAnsible],
+]:
     records = [
         Update.from_hypercore(hypercore_data=hypercore_dict).to_ansible()  # type: ignore
         for hypercore_dict in rest_client.list_records("/rest/v1/Update")
     ]
-    if module.params["select"]:
-        record = select_next_or_latest(records, module.params["select"])
-    return records, record
+    if records:
+        records.sort(
+            key=operator.itemgetter(
+                "major_version", "minor_version", "revision", "build_id"
+            )
+        )
+        return records, records[0], records[-1]
+    return records, None, None
 
 
 def main() -> None:
@@ -173,15 +184,14 @@ def main() -> None:
         supports_check_mode=True,
         argument_spec=dict(
             arguments.get_spec("cluster_instance"),
-            select=dict(type="str", choices=["next", "latest"]),
         ),
     )
 
     try:
         client = Client.get_client(module.params["cluster_instance"])
         rest_client = RestClient(client)
-        records, record = run(module, rest_client)
-        module.exit_json(changed=False, records=records, record=record)
+        records, next, latest = run(rest_client)
+        module.exit_json(changed=False, records=records, next=next, latest=latest)
 
     except errors.ScaleComputingError as e:
         module.fail_json(msg=str(e))
