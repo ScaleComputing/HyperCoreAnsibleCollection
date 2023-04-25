0d99d941e5ef5fdb98ac57c71c0528a3d93a41ce
Domen <domen.dobnikar@xlab.si>
2023-04-25 15:07:10 +0200

    vm snapshot unit test draft

--- tests/unit/plugins/modules/test_vm_snapshot.py
+++ tests/unit/plugins/modules/test_vm_snapshot.py
@@ -5,11 +5,13 @@ __metaclass__ = type
 import sys
 
 import pytest
+import datetime
 
 from ansible_collections.scale_computing.hypercore.plugins.modules import vm_snapshot
 from ansible_collections.scale_computing.hypercore.plugins.module_utils.vm_snapshot import (
     VMSnapshot,
 )
+from ansible_collections.scale_computing.hypercore.plugins.module_utils.vm import VM
 from ansible_collections.scale_computing.hypercore.plugins.module_utils.errors import (
     ScaleComputingError,
 )
@@ -35,11 +37,67 @@ class TestMain:
         ("vm_name", "label", "retain_for", "replication", "state", "expected_result"),
         [
             # Present
-            ("test-vm", "this-label", 10, True, "present", (True, dict(changed=False, record=dict(), diff=dict(before=dict(), after=dict())))),
-            ("test-vm", "this-label", 0, False, "present", (True, dict(changed=False, record=dict(), diff=dict(before=dict(), after=dict())))),
+            (
+                "test-vm",
+                "this-label",
+                10,
+                True,
+                "present",
+                (
+                    True,
+                    dict(
+                        changed=False,
+                        record=dict(),
+                        diff=dict(before=dict(), after=dict()),
+                    ),
+                ),
+            ),
+            (
+                "test-vm",
+                "this-label",
+                0,
+                False,
+                "present",
+                (
+                    True,
+                    dict(
+                        changed=False,
+                        record=dict(),
+                        diff=dict(before=dict(), after=dict()),
+                    ),
+                ),
+            ),
             # Absent
-            ("test-vm", "this-label", 10, True, "absent", (True, dict(changed=False, record=dict(), diff=dict(before=dict(), after=dict())))),
-            ("test-vm", "this-label", 15, False, "absent", (True, dict(changed=False, record=dict(), diff=dict(before=dict(), after=dict())))),
+            (
+                "test-vm",
+                "this-label",
+                10,
+                True,
+                "absent",
+                (
+                    True,
+                    dict(
+                        changed=False,
+                        record=dict(),
+                        diff=dict(before=dict(), after=dict()),
+                    ),
+                ),
+            ),
+            (
+                "test-vm",
+                "this-label",
+                15,
+                False,
+                "absent",
+                (
+                    True,
+                    dict(
+                        changed=False,
+                        record=dict(),
+                        diff=dict(before=dict(), after=dict()),
+                    ),
+                ),
+            ),
         ],
     )
     def test_parameters_snapshot(
@@ -70,13 +128,43 @@ class TestMain:
 
 # Test run() module function.
 class TestRun:
+    @pytest.mark.parametrize(
+        # state                             ... which state is sent to run()
+        # snapshot_list                     ... get_snapshot query return
+        # expected_exception                ... is get_snapshot exception expected
+        # expected_result                   ... expected run() return
+        ("state", "snapshot_list", "expected_exception", "expected_result"),
+        [
+            # Present; No exception.
+            ("present", [], False, (True, dict(), dict())),
+            ("present", [dict(label="this-label")], False, (False, dict(), dict())),
+            # Present; with exception.
+            (
+                "present",
+                [dict(label="this-label"), dict(label="this-label")],
+                True,
+                (False, dict(), dict()),
+            ),
+            # Absent; No exception.
+            ("absent", [], False, (False, dict(), dict())),
+            ("absent", [dict(label="this-label")], False, (True, dict(), dict())),
+            # Absent; with exception.
+            (
+                "absent",
+                [dict(label="this-label"), dict(label="this-label")],
+                True,
+                (False, dict(), dict()),
+            ),
+        ],
+    )
     def test_run_virtual_disk(
         self,
         create_module,
         rest_client,
         mocker,
-        virtual_disk_dict,
-        state_test,
+        state,
+        snapshot_list,
+        expected_exception,
         expected_result,
     ) -> None:
         module = create_module(
@@ -84,25 +172,210 @@ class TestRun:
                 cluster_instance=dict(
                     host="https://my.host.name", username="user", password="pass"
                 ),
-                name="foobar.qcow2",
-                source="c:/somewhere/foobar.qcow2",
-                state=state_test,
+                vm_name="this-VM",
+                label="this-label",
+                retain_for=30,
+                replication=True,
+                state=state,
             )
         )
-        # Does virtual_disk exist on cluster or not.
-        if virtual_disk_dict:
-            virtual_disk_obj = VirtualDisk.from_hypercore(virtual_disk_dict)
+
+        # Mock VM
+        mock_VM = mocker.MagicMock(spec=VM)
+        mock_VM.uuid = "123"
+        mocker.patch(
+            "ansible_collections.scale_computing.hypercore.plugins.module_utils.vm.VM.get_by_name"
+        ).return_value = mock_VM
+
+        # Mock get_snapshot
+        mocker.patch(
+            "ansible_collections.scale_computing.hypercore.plugins.module_utils.vm_snapshot.VMSnapshot.get_snapshots_by_query"
+        ).return_value = snapshot_list
+
+        # Check for snapshot exception.
+        if expected_exception:
+            with pytest.raises(
+                ScaleComputingError,
+                match=f"Virtual machine - {module.params['vm_name']} - has more than one snapshot with label - {module.params['label']}.",
+            ):
+                vm_snapshot.run(module, rest_client)
         else:
-            virtual_disk_obj = None
-        mocker.patch(
-            "ansible_collections.scale_computing.hypercore.plugins.module_utils.virtual_disk.VirtualDisk.get_by_name"
-        ).return_value = virtual_disk_obj
+            # Mock ensure_present and ensure_absent
+            mocker.patch(
+                f"ansible_collections.scale_computing.hypercore.plugins.modules.vm_snapshot.ensure_{state}"
+            ).return_value = expected_result
 
-        # Mock ensure_present or ensure_absent return value.
-        mocker.patch(
-            f"ansible_collections.scale_computing.hypercore.plugins.modules.virtual_disk.ensure_{state_test}"
-        ).return_value = expected_result
+            results = vm_snapshot.run(module, rest_client)
+            print(results, expected_result)
+            assert isinstance(results, tuple)
+            assert results == expected_result
 
-        results = vm_snapshot.run(module, rest_client)
-        assert isinstance(results, tuple)
-        assert results == expected_result
+
+# Test ensure_present() module function.
+class TestEnsurePresent:
+    @pytest.mark.parametrize(
+        # label                             ... Snapsho label
+        # snapshot_list                     ... get_snapshot query return
+        # after                             ... after returned by API
+        # expected_result                   ... expected resulted returned by function
+        ("label", "snapshot_list", "after", "expected_result"),
+        [
+            # Create snapshot.
+            (
+                "this-label",
+                [],
+                [VMSnapshot(vm_name="this-vm", label="this-label")],
+                (
+                    True,
+                    VMSnapshot(vm_name="this-vm", label="this-label"),
+                    dict(
+                        before=None,
+                        after=VMSnapshot(vm_name="this-vm", label="this-label"),
+                    ),
+                ),
+            ),
+            # Snapshot already exists, do nothing.
+            (
+                "this-label",
+                [VMSnapshot(vm_name="this-vm", label="this-label")],
+                [VMSnapshot(vm_name="this-vm", label="this-label")],
+                (
+                    False,
+                    VMSnapshot(vm_name="this-vm", label="this-label"),
+                    dict(
+                        before=VMSnapshot(vm_name="this-vm", label="this-label"),
+                        after=VMSnapshot(vm_name="this-vm", label="this-label"),
+                    ),
+                ),
+            ),
+        ],
+    )
+    def test_ensure_present_vm_snapshot(
+        self,
+        create_module,
+        rest_client,
+        mocker,
+        label,
+        snapshot_list,
+        after,
+        expected_result,
+    ):
+        # Mock module
+        module = create_module(
+            params=dict(
+                cluster_instance=dict(
+                    host="https://my.host.name", username="user", password="pass"
+                ),
+                vm_name="this-VM",
+                label=label,
+                retain_for=None,
+                replication=True,
+                state="present",
+            )
+        )
+        # Mock VM
+        vm_object = mocker.MagicMock(spec=VM)
+        vm_object.uuid = "123"
+
+        # Mock check_parameters
+        mocker.patch(
+            "ansible_collections.scale_computing.hypercore.plugins.modules.vm_snapshot.check_parameters"
+        ).return_value = module
+
+        # Mock send_create_request and task
+        task = dict()
+        mocker.patch(
+            "ansible_collections.scale_computing.hypercore.plugins.module_utils.vm_snapshot.VMSnapshot.send_create_request"
+        ).return_value = task
+        mocker.patch(
+            "ansible_collections.scale_computing.hypercore.plugins.module_utils.task_tag.TaskTag.wait_task"
+        ).return_value = None
+
+        # Mock after
+        mocker.patch(
+            "ansible_collections.scale_computing.hypercore.plugins.module_utils.vm_snapshot.VMSnapshot.get_snapshots_by_query"
+        ).return_value = after
+
+        result = vm_snapshot.ensure_present(
+            module, rest_client, vm_object, snapshot_list
+        )
+        assert isinstance(result, tuple)
+        assert result == expected_result
+
+
+# Test ensure_absent() module function.
+class TestEnsureAbsent:
+    @pytest.mark.parametrize(
+        # label                             ... Snapsho label
+        # snapshot_list                     ... get_snapshot query return
+        # after                             ... after returned by API
+        # expected_result                   ... expected resulted returned by function
+        ("label", "snapshot_list", "after", "expected_result"),
+        [
+            # Snapshot already absent, do nothing.
+            ("this-label", [], [], (False, None, dict(before=None, after=None))),
+            # Delete snapshot.
+            (
+                "this-label",
+                [dict(vm_name="this-vm", label="this-label", snapshot_uuid="123")],
+                [],
+                (
+                    True,
+                    None,
+                    dict(
+                        before=dict(
+                            vm_name="this-vm", label="this-label", snapshot_uuid="123"
+                        ),
+                        after=None,
+                    ),
+                ),
+            ),
+        ],
+    )
+    def test_ensure_absent_vm_snapshot(
+        self,
+        create_module,
+        rest_client,
+        mocker,
+        label,
+        snapshot_list,
+        after,
+        expected_result,
+    ):
+        # Mock module
+        module = create_module(
+            params=dict(
+                cluster_instance=dict(
+                    host="https://my.host.name", username="user", password="pass"
+                ),
+                vm_name="this-VM",
+                label=label,
+                retain_for=None,
+                replication=True,
+                state="absent",
+            )
+        )
+        # Mock VM
+        vm_object = mocker.MagicMock(spec=VM)
+        vm_object.uuid = "123"
+
+        # Mock send_delete_request and task
+        task = dict()
+        mocker.patch(
+            "ansible_collections.scale_computing.hypercore.plugins.module_utils.vm_snapshot.VMSnapshot.send_delete_request"
+        ).return_value = task
+        mocker.patch(
+            "ansible_collections.scale_computing.hypercore.plugins.module_utils.task_tag.TaskTag.wait_task"
+        ).return_value = None
+
+        # Mock after
+        mocker.patch(
+            "ansible_collections.scale_computing.hypercore.plugins.module_utils.vm_snapshot.VMSnapshot.get_snapshots_by_query"
+        ).return_value = after
+
+        result = vm_snapshot.ensure_absent(
+            module, rest_client, vm_object, snapshot_list
+        )
+        print(result, expected_result)
+        assert isinstance(result, tuple)
+        assert result == expected_result
